ff6a1db5e997c7f8ed1f06162e8a92a07cb012ba
youtangai <youtanagai@gmail.com>
2017-05-09 23:51:45 +0900

    window.openの引数を変更

--- www/js/common.js
+++ www/js/common.js
@@ -167,7 +167,7 @@ function ExportList(eventObj, participants) {
             .catch(function(err){
             // エラー処理
             });
-
     // Safariやchromeを用いて、生成したcsvを表示
-    window.open('https://mb.api.cloud.nifty.com/2013-09-01/applications/zMCtGPFYQE1JZ2UY/publicFiles/' + filename +'','_system','toolbarposition=top,suppressesIncrementalRendering=yes,location=no,closebuttoncaption=戻る');
+    //window.open('https://mb.api.cloud.nifty.com/2013-09-01/applications/zMCtGPFYQE1JZ2UY/publicFiles/' + filename +'','_system','toolbarposition=top,suppressesIncrementalRendering=yes,location=no,closebuttoncaption=戻る');
+    window.open('https://mb.api.cloud.nifty.com/2013-09-01/applications/zMCtGPFYQE1JZ2UY/publicFiles/' + filename +'','_system','location=no,presentationstyle=fullscreen,toolbar=no');
 }
